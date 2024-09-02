8dfb1e7ad4a2fc2f69be2290ce32b5c7b2346e9a
Mark Popa <popamarknicolas1@gmail.com>
2024-09-02 16:05:54 +0200

    commit

--- gameoflife.py
+++ gameoflife.py
@@ -5,7 +5,7 @@ import numpy as np
 COLOR_BG = (10, 10, 10)
 COLOR_GRID = (40, 40, 40)
 COLOR_DIE_NEXT = (170, 170, 170)
-COLOR_ALIVE = (255, 255, 255)
+COLOR_ALIVE_NEXT = (255, 255, 255)
 
 
 def update(screen, cells, size, with_progress=False):
@@ -13,4 +13,32 @@ def update(screen, cells, size, with_progress=False):
     
     for row, col in np.ndindex(cells.shape):
         alive = np.sum(cells[row-1:row+2, col-1:col+2]) - cells[row, col]
-        color 
+        color = COLOR_BG if cells[row, col] == 0 else COLOR_ALIVE_NEXT
+        
+        if cells[row, col]  == 1:
+            if alive < 2 or alive > 3:
+                if with_progress:
+                    color = COLOR_DIE_NEXT
+            elif 2 <= alive <= 3:
+                updated_cells[row, col] = 1
+                if with_progress:
+                    color = COLOR_ALIVE_NEXT
+        else:
+            if alive == 3:
+                updated_cells[row, col] = 1
+                if with_progress:
+                    color = COLOR_ALIVE_NEXT
+                    
+        pygame.draw.rect(screen, color, (col * size, row * size, size -1, size -1))
+        
+    return updated_cells
+
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((800, 600))
+    
+    cells = np.zeros((60, 80))
+    screen.fill(COLOR_GRID)
+    update(screen, cells, 10)
+    
