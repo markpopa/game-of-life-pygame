4bad540ba4c5f18ae4b920a02722c99a5f6a050d
Mark Popa <popamarknicolas1@gmail.com>
2024-09-03 14:26:39 +0200

    commit

--- gameoflife.py
+++ gameoflife.py
@@ -47,4 +47,29 @@ def main():
     
     running = False
     
-
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    running = not running
+                    update(screen, cells, 10)
+                    pygame.display.update()
+            if pygame.mouse.get_pressed()[0]:
+                pos = pygame.mouse.get_pos()
+                cells[pos[1] // 10, pos[0] // 10] = 1
+                update(screen, cells, 10)
+                pygame.display.update()
+                
+        screen.fill(COLOR_GRID)
+        
+        if running:
+            cells = update(screen, cells, 10, with_progress=True)
+            pygame.display.update()
+            
+        time.sleep(0.001)
+        
+if __name__ == '__main__':
+    main()
